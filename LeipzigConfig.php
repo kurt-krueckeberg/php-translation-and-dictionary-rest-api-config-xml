3daf89d9ba261672cb760fa8c57971f40f34caae
Kurt Kruecleberg <kurt.krueckeberg@gmail.com>
2022-04-24 14:59:36 -0400

    updates

--- LeipzigConfig.php
+++ LeipzigConfig.php
@@ -4,17 +4,16 @@ namespace LanguageTools;
 
 class LeipzigConfig implements ConfigInterface {
 
-
+     private string $endpoint = "http://api.corpora.uni-leipzig.de/ws";
 
   public function get_endpoint() : string
   {
-
+     return $this->endpoint;
   }
-
 
   public function get_authorization() : array | null
   {
-
+      return null;
   }
 }
 
