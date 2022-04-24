c4648ee748f6345c251a3f0f08948dabbec7f81e
Kurt Kruecleberg <kurt.krueckeberg@gmail.com>
2022-04-24 14:53:30 -0400

    updates

---
+++ IbmConfig.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+namespace LanguageTools;
+
+class IbmConfig implements ConfigInterface {
+
+
+
+  public function get_endpoint() : string
+  {
+
+  }
+
+
+  public function get_authorization() : array | null
+  {
+
+  }
+}
+
