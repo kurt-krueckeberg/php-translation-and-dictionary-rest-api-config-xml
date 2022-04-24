c3d5498a9fd5684b40f184dc062d81de970a6c7b
Kurt Kruecleberg <kurt.krueckeberg@gmail.com>
2022-04-24 15:03:24 -0400

    updates

--- AzureConfig.php
+++ AzureConfig.php
@@ -4,17 +4,17 @@ namespace LanguageTools;
 
 class AzureConfig implements ConfigInterface {
 
+  private string $endpoint = "https://api.cognitive.microsofttranslator.com";
+  private array $header = ["Ocp-Apim-Subscription-Key" => "ef6e5b44c68d438c8d79cae2f8c020ba", "Ocp-Apim-Subscription-Region" => "eastus2"];
 
 
   public function get_endpoint() : string
   {
-
+     return $this->endpoint;
   }
-
 
   public function get_authorization() : array | null
   {
-
+      return i$this->headers;
   }
 }
-
