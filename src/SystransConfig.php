714d8538122003ee02e26e985cf8e3469ebac0b5
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2022-04-27 15:21:01 -0400

    Update SystransConfig.php

--- src/SystransConfig.php
+++ src/SystransConfig.php
@@ -4,12 +4,9 @@ namespace LanguageTools;
 
 class SystransConfig {
  /*
-   These yet-to-be-defined readonly properties are assigned (and promoted to class member variables) on the constructor:
-
-      public readonly string $endpoint = 
-      prublic readonly array $header = 
+   Client ID: HJ4lFfM7gBmTbYJj0BNO5
+    Client Secret: mGsJki6GLCJfPS7c1U861cC_hrLJVGoSW-EPHrnb_sQpJfY9n4wjo_jQjY_Jk3_8dxCJcPemHx_NF4Syzf77-w
   */
   public function __construct(public readonly string $endpoint = "", 
-      public readonly array $header = array()) {}
+      public readonly array $header = array('HJ4lFfM7gBmTbYJj0BNO5' => 'mGsJki6GLCJfPS7c1U861cC_hrLJVGoSW-EPHrnb_sQpJfY9n4wjo_jQjY_Jk3_8dxCJcPemHx_NF4Syzf77-w')) {}
 }
-
