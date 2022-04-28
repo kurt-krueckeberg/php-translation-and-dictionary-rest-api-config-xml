a82bb4924ed96e43addd2d902aa5b2f7aefba338
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2022-04-28 09:20:08 -0400

    Add files via upload

--- src/SystranConfig.php
+++ src/SystranConfig.php
@@ -9,6 +9,6 @@ class SystranConfig {
    Per https://docs.systran.net/translateAPI/translation/#tag-Translation the Client ID is not passed; "Key" is passed before the actual key.
    But then what is the Cleint ID for?
   */
-  public function __construct(public readonly string $endpoint = "", 
-      public readonly array $header = array('Key' => 'mGsJki6GLCJfPS7c1U861cC_hrLJVGoSW-EPHrnb_sQpJfY9n4wjo_jQjY_Jk3_8dxCJcPemHx_NF4Syzf77-w')) {}
+  public function __construct(public readonly string $endpoint = "https://api-translate.systran.net",
+      public readonly array $header = array('Authorization' => 'Key 6b9632e2-d5b9-493c-a692-d71eda6e59e9')) {}
 }
