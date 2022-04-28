b4874a2be26d1b4bd2a992e9cbd1672da17a62e5
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2022-04-27 20:34:22 -0400

    Update SystranConfig.php

--- src/SystranConfig.php
+++ src/SystranConfig.php
@@ -5,8 +5,10 @@ namespace LanguageTools;
 class SystranConfig {
  /*
    Client ID: HJ4lFfM7gBmTbYJj0BNO5
-    Client Secret: mGsJki6GLCJfPS7c1U861cC_hrLJVGoSW-EPHrnb_sQpJfY9n4wjo_jQjY_Jk3_8dxCJcPemHx_NF4Syzf77-w
+   Client Secret: mGsJki6GLCJfPS7c1U861cC_hrLJVGoSW-EPHrnb_sQpJfY9n4wjo_jQjY_Jk3_8dxCJcPemHx_NF4Syzf77-w
+   Per https://docs.systran.net/translateAPI/translation/#tag-Translation the Client ID is not passed; "Key" is passed before the actual key.
+   But then what is the Cleint ID for?
   */
   public function __construct(public readonly string $endpoint = "", 
-      public readonly array $header = array('HJ4lFfM7gBmTbYJj0BNO5' => 'mGsJki6GLCJfPS7c1U861cC_hrLJVGoSW-EPHrnb_sQpJfY9n4wjo_jQjY_Jk3_8dxCJcPemHx_NF4Syzf77-w')) {}
+      public readonly array $header = array('Key' => 'mGsJki6GLCJfPS7c1U861cC_hrLJVGoSW-EPHrnb_sQpJfY9n4wjo_jQjY_Jk3_8dxCJcPemHx_NF4Syzf77-w')) {}
 }
