2864e35c6d7b3c09fbbf01a998d21168c10f5c7b
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2022-05-03 19:01:28 -0400

    Add files via upload

--- src/DeeplConfig.php
+++ src/DeeplConfig.php
@@ -3,15 +3,10 @@ declare(strict_types=1);
 namespace LanguageTools;
 
 class DeeplConfig {
- /*
-   These readonly properties are assigned (and promoted to class member variables) on the constructor:
-
-      public readonly string $endpoint = "https://api-free.deepl.com/v2";
-      prublic readonly array $header = ["DeepL-Auth-Key" => "7482c761-0429-6c34-766e-fddd88c247f9:fx"];
-  */
-
-   public __construct(public readonly string $endpoint = "https://api-free.deepl.com/v2",
-                      prublic readonly array $header = ["DeepL-Auth-Key" => "7482c761-0429-6c34-766e-fddd88c247f9:fx"]) {}
+    
+   public function __construct(
+                      public readonly array $header = [ "Authorization" => "DeepL-Auth-Key 7482c761-0429-6c34-766e-fddd88c247f9:fx"],
+                      public readonly string $endpoint = "https://api-free.deepl.com",
+                     ) {}
             
 }
-
