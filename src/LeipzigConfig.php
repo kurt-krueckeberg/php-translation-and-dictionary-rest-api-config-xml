9544a32acb238ea13c1f6f29d198c9cce1866542
kurt <kurt.krueckeberg@gmail.com>
2022-06-09 08:37:43 -0400

    updates

--- src/LeipzigConfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-namespace LanguageTools;
-
-class LeipzigConfig {
-  /* $endpoint is promoted to a public readonly class members on the constructor  */
-  public function __construct(public readonly string $endpoint = "http://api.corpora.uni-leipzig.de/ws") {}
-}
